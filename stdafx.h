0be042a0ab82bd1df563e916002d2cab5eb66bec
Hohn <Hohn@THEXP>
2017-09-12 23:39:21 +0900

    iro

--- stdafx.h
+++ stdafx.h
@@ -11,6 +11,35 @@
 #include <shlwapi.h>
 #pragma comment(lib, "shlwapi.lib")
 
+#include <comdef.h> 
+#include <tchar.h>
+#include <objbase.h>
+#include <shlobj.h>
+#include <ShObjIdl.h>
+#include <vcclr.h>
+#include <string>
+#include <io.h>
+#include <windows.h>
+#include <tchar.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <windows.h>
+#include <tchar.h>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#include <string>
+
+#ifdef _DEBUG
+#include <cassert>
+#endif
+
+#include <stlsoft/smartptr/scoped_handle.hpp>
+
 
 #include "../lsMisc/DebugMacro.h"
 #include "../lsMisc/IsFilenamable.h"
