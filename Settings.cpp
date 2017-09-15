68340a6f879b10de0574c6fb10c6b18ee6980528
zelda <zelda@THEXP>
2017-09-15 18:55:49 +0900

    many

--- Settings.cpp
+++ Settings.cpp
@@ -1,3 +1,26 @@
+//Copyright (C) 2017 Ambiesoft All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions
+//are met:
+//1. Redistributions of source code must retain the above copyright
+//notice, this list of conditions and the following disclaimer.
+//2. Redistributions in binary form must reproduce the above copyright
+//notice, this list of conditions and the following disclaimer in the
+//documentation and/or other materials provided with the distribution.
+//
+//THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+//ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+//FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+//OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+//OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+//SUCH DAMAGE.
+
 #include "stdafx.h"
 #include "Settings.h"
 
@@ -29,7 +52,7 @@ namespace Ambiesoft {
 			sb.AppendLine(I18N(L"Defaultpath: Default relative folder path from exe-resident folder. (Default is Exe-resident folder)"));
 			sb.AppendLine(I18N(L"Creator: Creator name used as subfolder of roaming or local folder."));
 			sb.AppendLine(I18N(L"Appname: App name used as subfolder of Creator."));
-			sb.AppendLine(I18N(L"Section: Section name of config. (Default is FolderConfig)"));
+			sb.AppendLine(I18N(L"Section: Section name of config. (Default is [Main])"));
 			sb.AppendLine(I18N(L"Culture: Culture string, (ex) \"ja-JP\"."));
 
 			return sb.ToString();
@@ -266,7 +289,7 @@ namespace Ambiesoft {
 			}
 			if (String::IsNullOrEmpty(Settings::Section))
 			{
-				Settings::section_ = L"FolderConfig";
+				Settings::section_ = Settings::section_default_;
 			}
 
 
